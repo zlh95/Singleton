1822a95b541c4782a96a57ce34ff8b4cb16673d5
zlh95 <346767586@qq.com>
2018-09-18 16:56:16 +0800

    注释

--- Singleton1.py
+++ Singleton1.py
@@ -1,5 +1,10 @@
 class Singlet:
     def __new__(cls, *args, **kwargs):
+        '''
+        __new__()是真正的构造方法，它是一个类方法，
+        必须返回一个实例，返回的实例会作为第一个参数（self）
+        传给__init__()方法。__init__()方法实际上是初始化方法。
+        '''
         if not hasattr(cls,'instance'):
             cls.instance = super().__new__(cls,*args,**kwargs)
         return cls.instance
--- Singleton2.py
+++ Singleton2.py
@@ -1,7 +1,7 @@
 class Borg(object):
     _state = {}
     def __new__(cls, *args, **kw):
-        ob = super(Borg, cls).__new__(cls, *args, **kw)
+        ob = super().__new__(cls, *args, **kw)  #将方法调用委托给超类
         ob.__dict__ = cls._state
         return ob.__dict__
 
